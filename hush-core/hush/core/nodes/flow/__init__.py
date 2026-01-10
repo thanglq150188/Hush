83fc04274be0c1d185f4b75227edfd6db26809b1
Le Quang Thang <thanglq150188@gmail.com>
2026-01-11 02:29:01 +0700

    fix branch node for easier usage

--- hush-core/hush/core/nodes/flow/__init__.py
+++ hush-core/hush/core/nodes/flow/__init__.py
@@ -2,10 +2,12 @@
 
 Bao gồm:
 - BranchNode: Định tuyến có điều kiện dựa trên đánh giá expression
+- Branch: Fluent builder để tạo BranchNode với cú pháp tự nhiên hơn
 """
 
-from .branch_node import BranchNode
+from .branch_node import BranchNode, Branch
 
 __all__ = [
-    "BranchNode"
+    "BranchNode",
+    "Branch"
 ]
